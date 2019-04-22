010b09978d8feed8091bfc181988dc8b04640a9f
Saltuk Kezer <saltukke@gmail.com>
2019-04-22 10:56:30 +0200

    Clean-up and syncing parts moving

--- Snake.py
+++ Snake.py
@@ -1,102 +1,89 @@
-"""
- Pygame base template for opening a window
-
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
-
- Explanation video: http://youtu.be/vRB_983kUMc
-"""
 from enum import Enum
 
 import pygame
 import random
-
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0,0,255)
 
 class Direction():
     def __init__(self, dx, dy):
         self.dx = dx
         self.dy = dy
 
+class Part():
+    SIZE = 30
+
+    def __init__(self,direction):
+        self.direction = direction
+        self.nextDirection = None
+        self.position = (0,0)
+
+    def update(self):
+        self.position = (self.position[0] + self.direction.dx, self.position[1] + self.direction.dy)
+        
+        if self.nextDirection is not None:
+            self.direction = self.nextDirection
+            self.nextDirection = None
+
+
+    def render(self, screen, progress):
+        px = self.position[0] + progress * self.direction.dx
+        py = self.position[1] + progress * self.direction.dy
+
+        pygame.draw.ellipse(screen, RED, [px* Part.SIZE, py * Part.SIZE, Part.SIZE, Part.SIZE])
+
+#Directions
 UP = Direction(0,-1)
 RIGHT = Direction(1,0)
 DOWN = Direction(0,1)
 LEFT = Direction(-1,0)
 
-class Part():
-    SIZE = 30
+#Colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255, 0, 0)
+BLUE = (0,0,255)
 
-    def __init__(self):
-        self.direction = RIGHT
-        self.nextDirection = None
-        self.progress = 0
-        self.position = [0,0]
-
-    def update(self, delta):
-        self.progress += delta * 15
-        
-        if self.progress >= 1:
-            while self.progress >= 1:
-                self.position[0] += self.direction.dx
-                self.position[1] += self.direction.dy
-                self.progress -= 1
-
-            if self.nextDirection is not None:
-                self.direction = self.nextDirection
-                self.nextDirection = None
-
-
-    def render(self, screen):
-        px = self.position[0] + self.progress * self.direction.dx
-        py = self.position[1] + self.progress * self.direction.dy
-
-        pygame.draw.ellipse(screen, RED, [px* Part.SIZE, py * Part.SIZE, Part.SIZE, Part.SIZE])
-
-pygame.init()
-
-# Set the width and height of the screen [width, height]
+# Screen-related variables
 size = (600, 600)
+pwidth = size[0]
+pheight = size[1]
 screen = pygame.display.set_mode(size)
 
+#pygame
 pygame.display.set_caption("Snake")
+pygame.init()
 
-# Loop until the user clicks the close button.
+# Game-logic variables
 done = False
 gameOver = False
-
-# Used to manage how fast the screen updates
+progress = 0
 clock = pygame.time.Clock()
 
+# Game-logic (snake) variables
 parts = []
-head = Part()
-head.position = [3,0]
+head = Part(RIGHT)
+head.position = (0,0)
 parts.append(head)
 
+# Game-logic (food) variables
+foodX = random.randint(0, int(pwidth/Part.SIZE) - 1)
+foodY = random.randint(0, int(pheight/Part.SIZE) - 1)
+food = (foodX, foodY)
+
+#Rendering variables
+font = pygame.font.SysFont('Calibri', 30)
+
+# Global method
 def addPart():
-    last = parts[len(parts)-1]
-    part = Part()
+    last = parts[-1]
+    lastDirection = last.direction
+    part = Part(lastDirection)
     
-    part.position[0] = last.position[0] - 1
-    part.position[1] = last.position[1] 
+    part.position = (last.position[0] - lastDirection.dx, last.position[1] - lastDirection.dy )
     parts.append( part )
 
-for i in range(3):
-    addPart()
-
-foodX = random.randint(0, int(600/Part.SIZE) -1)
-foodY = random.randint(0, int(600/Part.SIZE) -1)
-food = [foodX, foodY]
-
-# -------- Main Program Loop -----------
+# Game loop
 while not done:
-    # --- Main event loop
+    # Event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
@@ -113,52 +100,58 @@ while not done:
             elif event.key == pygame. K_DOWN:
                 if head.direction is not UP:
                     head.nextDirection = DOWN
-                
-    # --- Game logic should go here
-    for i in range( 1, len(parts) ):
-        parts[ i ].nextDirection = parts[ i - 1 ].direction
-
-
-    if head.position[0] == food[0] and head.position[1] == food[1]:
-        addPart()
-        foodX = random.randint(0, int(600 / Part.SIZE) -1)
-        foodY = random.randint(0, int(600 / Part.SIZE) -1)
-        food = [foodX, foodY]
-
-    for part in parts:
-        width = int(600/Part.SIZE) - 1
-        height = int(600/Part.SIZE) - 1
-
-        part.position[0] %= width
-        part.position[1] %= height
-
-    for i in range (1, len(parts)):
-        if head.position[0] == parts[i].position[0] and head.position[1] == parts[i].position[1]:
-            gameOver = True
-
-
-    for part in parts:
-        part.update(1.0/60)
     
-    # --- Screen-clearing code goes here
+    if not gameOver:
+        # Main logic
+        for i in range( 1, len(parts) ):
+            parts[ i ].nextDirection = parts[ i - 1 ].direction
 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
 
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
+        # Collision and bounds check
+        if head.position == food:
+            addPart()
+            foodX = random.randint(0, int(pwidth / Part.SIZE) - 1)
+            foodY = random.randint(0, int(pheight / Part.SIZE) - 1)
+            food = (foodX, foodY)
+
+        for part in parts:
+            width = int(pwidth/Part.SIZE)
+            height = int(pheight/Part.SIZE)
+
+            part.position = (part.position[0] % width, part.position[1] % height)
+        
+        for i in range (1, len(parts)):
+            if head.position == parts[i].position:
+                gameOver = True
+            
+        # Updating part positions
+        delta = 1.0 / 60
+        progress += delta * 15
+        
+        if progress >= 1:
+            while progress >= 1:
+                progress -= 1
+            
+                for part in parts:
+                    part.update()
+    
+    # Rendering
     screen.fill(WHITE)
 
-    # --- Drawing code should go here
+    # Main render part    
     for part in parts:
-        part.render(screen)
+        part.render(screen, progress)
     pygame.draw.ellipse(screen,BLUE,[food[0] * Part.SIZE, food[1] * Part.SIZE, Part.SIZE, Part.SIZE])
-
-    # --- Go ahead and update the screen with what we've drawn.
+    
+    if(gameOver):
+        alphaSurface = pygame.Surface((pwidth,pheight), pygame.SRCALPHA)
+        alphaSurface.fill((0,0,0,150))                         
+        screen.blit(alphaSurface, (0,0))
+        textSurface = font.render('Verloren...', False, WHITE)
+        screen.blit(textSurface, (pwidth / 2 - textSurface.get_width() / 2, pheight / 2 - textSurface.get_height() / 2))
+    
     pygame.display.flip()
 
-    # --- Limit to 60 frames per second
     clock.tick(60)
     
-# Close the window and quit.
 pygame.quit()
